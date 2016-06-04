61971a03c904685e3ff699935b93185b73cc92ac
zhoujun <dev.zhoujun@gmail.com>
2016-06-04 14:34:57 +0800

    Sum Root to Leaf Numbers

---
+++ 129.sum-root-to-leaf-numbers_test.go
@@ -0,0 +1,60 @@
+package leetcode
+
+import (
+	"strconv"
+	"testing"
+)
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func sumNumbers(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	sum := 0
+	for _, n := range pathNumber(root) {
+		v, _ := strconv.Atoi(n)
+		sum += v
+	}
+	return sum
+}
+
+func pathNumber(root *TreeNode) (nums []string) {
+	var number = map[int]string{
+		0: "0", 1: "1", 2: "2", 3: "3", 4: "4", 5: "5", 6: "6", 7: "7", 8: "8", 9: "9",
+	}
+
+	val := number[root.Val]
+	if root.Left == nil && root.Right == nil {
+		return []string{val}
+	}
+	if root.Left != nil {
+		for _, n := range pathNumber(root.Left) {
+			nums = append(nums, val+n)
+		}
+	}
+	if root.Right != nil {
+		for _, n := range pathNumber(root.Right) {
+			nums = append(nums, val+n)
+		}
+	}
+	return
+}
+
+func TestSumNumbers(t *testing.T) {
+	n := &TreeNode{
+		Val:   1,
+		Left:  &TreeNode{Val: 2},
+		Right: &TreeNode{Val: 3},
+	}
+	sum := sumNumbers(n)
+	if sum != 25 {
+		t.Fatalf("%d != 25", sum)
+	}
+}
