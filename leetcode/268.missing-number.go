62b084e127bf9d40ea4817f860daf04275ffe403
zhoujun <dev.zhoujun@gmail.com>
2016-07-16 23:44:49 +0800

    Missing Number

--- leetcode/268.missing-number.go
+++ leetcode/268.missing-number.go
@@ -3,10 +3,17 @@
 package leetcode
 
 func missingNumber(nums []int) int {
-	n := len(nums)
-	s1, s2 := 0, (1+n)*n/2
-	for _, n := range nums {
-		s1 += n
+	/*
+		n := len(nums)
+		s1, s2 := 0, (1+n)*n/2
+		for _, n := range nums {
+			s1 += n
+		}
+		return s2 - s1
+	*/
+	sum := len(nums)
+	for i, n := range nums {
+		sum += i - n
 	}
-	return s2 - s1
+	return sum
 }
