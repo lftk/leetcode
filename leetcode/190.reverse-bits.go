a52e9fb42936828a6bf0fa5334c7de155aa8453d
zhoujun <dev.zhoujun@gmail.com>
2016-09-22 22:41:06 +0800

    Reverse Bits

--- leetcode/190.reverse-bits.go
+++ leetcode/190.reverse-bits.go
@@ -8,8 +8,9 @@ package leetcode
 typedef unsigned int uint32_t;
 
 uint32_t reverseBits(uint32_t n) {
+    int i;
     uint32_t m = 0;
-    for (int i = 0; i < 32; i++) {
+    for (i = 0; i < 32; i++) {
         m = (m << 1) | (n & 1);
         n = n >> 1;
     }
