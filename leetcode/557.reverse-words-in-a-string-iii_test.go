3424aff30c16ce6ba3eac6a03e521232dfe06fb4
zhoujun <dev.zhoujun@gmail.com>
2017-05-02 22:50:21 +0800

    Reverse Words in a String III

--- leetcode/557.reverse-words-in-a-string-iii_test.go
+++ leetcode/557.reverse-words-in-a-string-iii_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func Test_reverseWords3(t *testing.T) {
-	s1 := reverseString("Let's take LeetCode contest")
+	s1 := reverseWords3("Let's take LeetCode contest")
 	if s1 != "s'teL ekat edoCteeL tsetnoc" {
 		t.Error(s1, "s'teL ekat edoCteeL tsetnoc")
 		return
